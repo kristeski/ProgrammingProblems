21974e7b311dec5e1a52ce4a6268f3a3df3067b7
Kristijan <kiko.rici.2007@gmail.com>
2025-11-14 11:42:39 +0100

    Time: 211 ms (5.18%) | Memory: 241.6 MB (5.06%) - LeetSync

---
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.cpp
@@ -0,0 +1,28 @@
+class TreeAncestor {
+public:
+    vector<vector<int>> up;
+    TreeAncestor(int n, vector<int>& parent) {
+        up.resize(5e4+1, vector<int>(17));
+        for(int i = 0; i < n; i++)
+            up[i][0] = parent[i];
+        for(int j = 1; j < 17; j++) 
+            for(int i = 0; i < n; i++) {
+                if(up[i][j - 1] == -1)
+                    up[i][j] = -1;
+                else
+                    up[i][j] = up[ up[i][j - 1] ][j - 1];
+            }
+    }
+    
+    int getKthAncestor(int node, int k) {
+        while(k) {
+            int msb = (31 - __builtin_clz(k));
+            node = up[node][msb];
+            if(node == -1)
+                return node;
+            k ^= (1 << msb);
+        }
+        return node;
+    }
+};
+
