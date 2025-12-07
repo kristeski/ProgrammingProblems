300ecdbdec3b5556b87a9353ce6b9ba0a42c729a
Kristijan <kiko.rici.2007@gmail.com>
2025-12-07 16:21:53 +0100

    Time: 31 ms (25.27%) | Memory: 60.7 MB (8.49%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    long long maxRunTime(int n, vector<int>& batteries) {
-        int m = batteries.size();
-        if (m < n) return 0;
+    long long maxRunTime(int n, vector<int>& b) {
+        sort(b.begin(), b.end());
+        vector<int> l = vector<int>(b.begin() + b.size() - n, b.end());
+        long long extra = 0, rez = *l.begin();
+        int indx = 0;
+        for(int i = 0; i < b.size() - n; i++)
+            extra += b[i];
 
-        sort(batteries.begin(), batteries.end());
-
-        long long extra = 0;
-        for (int i = 0; i < m - n; ++i) {
-            extra += batteries[i];
-        }
-
-        vector<int> live(batteries.end() - n, batteries.end());
-
-        for (int i = 0; i < n - 1; ++i) {
-            long long need = (long long)(i + 1) * (live[i + 1] - live[i]);
-            if (extra < need) {
-                return (long long)live[i] + extra / (i + 1);
+        for(int i = 1; i < n; i++) {
+            int val = (l[i] - l[i - 1]);
+            if(extra >= i * val) {
+                extra -= i * val;
+                rez += val;
+                indx = i;
             }
-            extra -= need;
+            else 
+                break;
         }
-
-        return (long long)live[n - 1] + extra / n;
+        rez += extra / (indx + 1);
+        return rez;
     }
 };
