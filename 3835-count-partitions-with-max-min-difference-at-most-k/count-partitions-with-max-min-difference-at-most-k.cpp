5165fa52b6873ab504e3a4f8dcdb238931c994ab
Kristijan <kiko.rici.2007@gmail.com>
2025-12-07 23:09:20 +0100

    Time: 1300 ms (5.13%) | Memory: 184.1 MB (52.85%) - LeetSync

--- 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.cpp
+++ 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.cpp
@@ -1,39 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    int countPartitions(vector<int>& nums, int k) {
-        int n = nums.size();
-        long long mod = 1e9 + 7;
-        vector<long long> dp(n + 1);
-        vector<long long> prefix(n + 1);
-        deque<int> minQ, maxQ;
+    const int M = 1e9 + 7;
 
+    struct Nd {
+        int maxi;
+        int mini;
+        int lo;
+    };
+
+    vector<Nd> st;
+    vector<int> nums;
+    int n;
+    int ni;
+
+
+    void build_tree(const vector<int>& a) {
+        nums = a;
+        n = (int)nums.size();
+        ni = 1;
+        while (ni < n) ni <<= 1;
+        st.assign(2 * ni, { INT_MIN, INT_MAX, INT_MAX });
+
+        for (int i = 1; i <= n; ++i) {
+            int idx = ni + (i - 1);
+            st[idx].maxi = nums[i - 1];
+            st[idx].mini = nums[i - 1];
+            st[idx].lo = i;
+        }
+
+        for (int v = ni - 1; v >= 1; --v) {
+            st[v].maxi = max(st[2 * v].maxi, st[2 * v + 1].maxi);
+            st[v].mini = min(st[2 * v].mini, st[2 * v + 1].mini);
+            st[v].lo = (st[2 * v].lo != INT_MAX) ? st[2 * v].lo : st[2 * v + 1].lo;
+        }
+    }
+
+    int range_max(int l, int r) {
+        if (l > r) return INT_MIN;
+        int L = ni + (l - 1);
+        int R = ni + (r - 1);
+        int res = INT_MIN;
+        while (L <= R) {
+            if (L & 1) res = max(res, st[L++].maxi);
+            if (!(R & 1)) res = max(res, st[R--].maxi);
+            L >>= 1; R >>= 1;
+        }
+        return res;
+    }
+
+    int range_min(int l, int r) {
+        if (l > r) return INT_MAX;
+        int L = ni + (l - 1);
+        int R = ni + (r - 1);
+        int res = INT_MAX;
+        while (L <= R) {
+            if (L & 1) res = min(res, st[L++].mini);
+            if (!(R & 1)) res = min(res, st[R--].mini);
+            L >>= 1; R >>= 1;
+        }
+        return res;
+    }
+
+    int climb(int pos, int k) {
+        int lo = 1, hi = pos;
+        while (lo < hi) {
+            int mid = (lo + hi) >> 1;
+            int mx = range_max(mid, pos);
+            int mn = range_min(mid, pos);
+            if ((long long)mx - (long long)mn <= k) hi = mid;
+            else lo = mid + 1;
+        }
+        return lo;
+    }
+
+    int countPartitions(vector<int>& a, int k) {
+        if (a.empty()) return 0;
+        build_tree(a);
+
+        vector<int> dp(n + 1, 0);
+        vector<int> pref(n + 1, 0);
         dp[0] = 1;
-        prefix[0] = 1;
-        for (int i = 0, j = 0; i < n; i++) {
+        pref[0] = 1;
 
-            while (!maxQ.empty() && nums[maxQ.back()] <= nums[i]) {
-                maxQ.pop_back();
-            }
-            maxQ.push_back(i);
-
-            while (!minQ.empty() && nums[minQ.back()] >= nums[i]) {
-                minQ.pop_back();
-            }
-            minQ.push_back(i);
-
-            while (!maxQ.empty() && !minQ.empty() &&
-                   nums[maxQ.front()] - nums[minQ.front()] > k) {
-                if (maxQ.front() == j) {
-                    maxQ.pop_front();
-                }
-                if (minQ.front() == j) {
-                    minQ.pop_front();
-                }
-                j++;
-            }
-
-            dp[i + 1] = (prefix[i] - (j > 0 ? prefix[j - 1] : 0) + mod) % mod;
-            prefix[i + 1] = (prefix[i] + dp[i + 1]) % mod;
+        for (int i = 1; i <= n; ++i) {
+            int j = climb(i, k);
+            long long total = pref[i - 1];
+            long long subtract = (j - 2 >= 0) ? pref[j - 2] : 0;
+            long long ways = (total - subtract) % M;
+            if (ways < 0) ways += M;
+            dp[i] = (int)ways;
+            pref[i] = pref[i - 1] + dp[i];
+            if (pref[i] >= M) pref[i] -= M;
         }
 
         return dp[n];
