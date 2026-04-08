3fe3d1498d39ba5e783ca766dac267af77313344
Kristijan <kiko.rici.2007@gmail.com>
2026-04-08 18:08:11 +0200

    Time: 59 ms (68.90%) | Memory: 158.1 MB (49.28%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
         const int M = 1000000007;
-        for(auto q : queries) {
-            int i = q[0], x = q[1], k = q[2], val = q[3];
+        for(const auto &q : queries) {
+            int i = q[0];
+            const int x = q[1];
+            const int k = q[2];
+            const int val = q[3];
             for(; i <= x; i += k) 
                 nums[i] = (1LL * nums[i] * val) % M;
         }
